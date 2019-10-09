45bde1cbc63b6154e0dfff083517098e67cf354d
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-10-09 14:46:59 +0200

    install

---
+++ MavenOCR/demo/src/main/java/fr/openclassroom/demo/App.java
@@ -0,0 +1,14 @@
+package fr.openclassroom.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Niort!" );
+        System.out.println("Hello Bessines mercredi 9 octobre 2019 - Achem");
+    }
+}
